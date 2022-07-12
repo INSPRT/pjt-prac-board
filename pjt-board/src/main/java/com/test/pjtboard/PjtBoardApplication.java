f0efe9a4f831e224ebdea007a7b5276ce70ee7a6
KTKT <a-b-c@kakao.com>
2022-07-12 10:18:54 +0900

    스프링 부트 프로젝트 시작
    * 스프링웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ pjt-board/src/main/java/com/test/pjtboard/PjtBoardApplication.java
@@ -0,0 +1,13 @@
+package com.test.pjtboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjtBoardApplication.class, args);
+    }
+
+}
---
+++ pjt-board/src/test/java/com/test/pjtboard/PjtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.pjtboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjtBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
